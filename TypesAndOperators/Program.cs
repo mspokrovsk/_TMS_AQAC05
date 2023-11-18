fc3cf5cc20445a16d02d9b83a35f630fb6e9a99d
mspokrovsk <enotestvaty@gmail.com>
2023-11-18 15:38:38 +0500

    Add Задание 3 + редактирование Заданий 1 и 2

--- TypesAndOperators/Program.cs
+++ TypesAndOperators/Program.cs
@@ -1,77 +1,130 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Задание 1
-  /*  double number1;
-    double number2;
-    string operation;
-    double result;
-    Console.Write("Введите первое число: ");
-    number1 = Convert.ToDouble(Console.ReadLine());
-    Console.Write("Введите одну из допустимых операций(+, -, *, /): ");
-    operation = Console.ReadLine();
-    Console.Write("Введите второе число: ");
-    number2 = Convert.ToDouble(Console.ReadLine());
+/*double number1;
+  double number2;
+  string operation;
+  double result;
+  Console.Write("Введите первое число: ");
+  number1 = Convert.ToDouble(Console.ReadLine());
+  Console.Write("Введите одну из допустимых операций(+, -, *, /): ");
+  operation = Console.ReadLine();
+  Console.Write("Введите второе число: ");
+  number2 = Convert.ToDouble(Console.ReadLine());
 
-    switch (operation)
-    {
-        case "+":
-            {
-                result = number1 + number2;
-                Console.WriteLine("Результат равен: " + result);
-                break;
-            }
-        case "-":
-            {
-                result = number1 - number2;
-                Console.WriteLine("Результат равен: " + result);
-                break;
-            }
-        case "*":
-            {
-                result = number1 * number2;
-                Console.WriteLine("Результат равен: " + result);
-                break;
-            }
-        case "/":
-            {
-                if (number2 == 0)
-                {
-                    Console.WriteLine("Ошибка: на 0 делить нельзя");
-                }
-                else
-                {
-                    result = number1 / number2;
-                    Console.WriteLine("Результат равен: " + result);
-                }
-                break;
-            }
-        default:
-            {
-                Console.WriteLine("Ошибка: Вы ввели недопустимую операцию");
-                break;
-            }
+  switch (operation)
+  {
+      case "+":
+          {
+              result = number1 + number2;
+              
+              break;
+          }
+      case "-":
+          {
+              result = number1 - number2;
+              
+              break;
+          }
+      case "*":
+          {
+              result = number1 * number2;
+              
+              break;
+          }
+      case "/":
+          {
+              if (number2 == 0)
+              {
+                  Console.WriteLine("Ошибка: на 0 делить нельзя");
+              }
+              else
+              {
+                  result = number1 / number2;
+                  
+              }
+              return;
+          }
+      default:
+          {
+              Console.WriteLine("Ошибка: Вы ввели недопустимую операцию");
+              return;
+          }
+Console.WriteLine("Результат равен: " + result);
 
-    }*/
+  }*/
 //Задание 2
+/*int number;
 Console.WriteLine("Введите число от -50 до 50:");
-int number = Convert.ToInt32(Console.ReadLine());
+number = Convert.ToInt32(Console.ReadLine());
 
 if (number >= -40 && number <= -10)
-{
+    {
     Console.WriteLine("Число входит в промежуток [-40, -10]");
-}
+    }
 else if (number >= -9 && number <= 0)
-{
+    {
     Console.WriteLine("Число входит в промежуток [-9, 0]");
-}
+    }
 else if (number >= 1 && number <= 10)
-{
+    {
     Console.WriteLine("Число входит в промежуток [1, 10]");
-}
+    }
 else if (number >= 11 && number <= 40)
-{
+    {
     Console.WriteLine("Число входит в промежуток [11, 40]");
-}
+    }
 else
 {
     Console.WriteLine("Число не входит ни в один из промежутков");
+}*/
+//Задание 3
+/*string[] word = {"отличная", "ужасная", "приятная", "переменчивая", "неустойчивая", "чудесная", "суровая", "непредсказуемая", "отвратительная", "мерзкая" };
+string word3;
+string translation = "";
+Console.WriteLine("Я русско-английский переводчик и я знаю всего 10 слов о погоде:");
+foreach (string word2 in word) //пробую практиковать параллельно с лекцией
+{
+    Console.WriteLine($"{word2} ");
 }
+Console.Write("Введите слово на русском языке: ");
+word3 = Console.ReadLine();
+
+switch (word3)
+{
+    case "отличная":
+        translation = "excellent";
+        break;
+    case "ужасная":
+        translation = "terrible";
+        break;
+    case "приятная":
+        translation = "nice";
+        break;
+    case "переменчивая":
+        translation = "changeable";
+        break;
+    case "неустойчивая":
+        translation = "unsettled";
+        break;
+    case "чудесная":
+        translation = "superb";
+        break;
+    case "суровая":
+        translation = "inclement";
+        break;
+    case "непредсказуемая":
+        translation = "unpredictable";
+        break;
+    case "отвратительная":
+        translation = "miserable";
+        break;
+    case "мерзкая":
+        translation = "foul";
+        break;
+    default:
+        Console.WriteLine("Ошибка: я не знаю это слово");
+        return;
+}
+
+Console.WriteLine("Перевод: " + translation);*/
+//Задание 4
