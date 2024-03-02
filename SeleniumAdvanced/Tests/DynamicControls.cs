396f101a0d9c0732baf1561ced2299ea4202f557
mspokrovsk <enotestvaty@gmail.com>
2024-03-02 20:16:22 +0500

    Add HW

---
+++ SeleniumAdvanced/Tests/DynamicControls.cs
@@ -0,0 +1,44 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using SeleniumBasic.Helpers;
+using System.Linq.Expressions;
+
+namespace SeleniumBasic.Tests
+{
+    public class DynamicControls : BaseTest
+    {
+        [Test]
+        public void TestDynamicControls()
+        {
+            Driver.Navigate().GoToUrl("http://the-internet.herokuapp.com/dynamic_controls");
+            // Нажимаем на кнопку Remove около чекбокса
+            IWebElement checkbox = WaitsHelper.WaitForVisibilityLocatedBy(By.CssSelector("input[type='checkbox']"));
+            IWebElement removeButton = Driver.FindElement(By.CssSelector("#checkbox-example button"));
+            removeButton.Click();
+
+            // Дожидаемся надписи "It’s gone"
+            IWebElement message = WaitsHelper.FluentWaitForElement(By.Id("message"));
+            Assert.That(message.Text, Is.EqualTo("It's gone!"));
+
+            // Проверяем, что чекбокса нет
+            
+            Assert.That(WaitsHelper.WaitForElementInvisible(checkbox));
+
+            // Находим input и проверяем, что он disabled
+            IWebElement input = Driver.FindElement(By.CssSelector("#input-example input"));
+            Assert.That(!input.Enabled);
+
+            // Нажимаем на кнопку
+            IWebElement enableButton = Driver.FindElement(By.CssSelector("#input-example button"));
+            enableButton.Click();
+
+            // Дожидаемся надписи "It's enabled!"
+            IWebElement messageEnabled = WaitsHelper.WaitForVisibilityLocatedBy(By.Id("message"));
+            Assert.That(messageEnabled.Text, Is.EqualTo("It's enabled!")); 
+
+            // Проверяем, что input enabled
+            Assert.That(input.Enabled);
+        }
+    }
+}
---
+++ SeleniumAdvanced/Tests/FileUpload.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumBasic.Tests
+{
+    public class FileUploadTest : BaseTest
+    {
+        [Test]
+        public void TestFileUpload()
+        {
+            Driver.Navigate().GoToUrl("http://the-internet.herokuapp.com/upload");
+
+            // Находим элемент для загрузки файла и отправляем путь к файлу
+            IWebElement fileInput = Driver.FindElement(By.Id("file-upload"));
+            string assemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string filePath = Path.Combine(assemblyPath, "Resources", "Spring - Sandro Botticelli.png");
+            fileInput.SendKeys(filePath);
+
+            // Нажимаем кнопку загрузки файла
+            IWebElement uploadButton = Driver.FindElement(By.Id("file-submit"));
+            uploadButton.Click();
+
+            // Проверяем, что имя файла на странице совпадает с именем загруженного файла
+            IWebElement uploadedFileName = Driver.FindElement(By.Id("uploaded-files"));
+            Assert.That(uploadedFileName.Text, Is.EqualTo("Spring - Sandro Botticelli.png"));
+        }
+    }
+}
