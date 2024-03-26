ac02953144cf189d5e8945e479637fe3f7bbf578
mspokrovsk <enotestvaty@gmail.com>
2024-03-26 18:57:22 +0500

    Update

--- Wrappers/Elements/UIElement.cs
+++ Wrappers/Elements/UIElement.cs
@@ -7,47 +7,47 @@ using Wrappers.Helpers.Configuration;
 
 namespace Wrappers.Elements;
 
-public class UIElement : IWebElement // Îáúÿâëåíèå êëàññà UIElement, ðåàëèçóþùåãî èíòåðôåéñ IWebElement 
+public class UIElement : IWebElement 
 {
-    //çàäàåì ïåðåìåííûå
-    private IWebDriver _webDriver; // Ïîëå äëÿ õðàíåíèÿ ýêçåìïëÿðà âåá-äðàéâåðà
-    private WaitsHelper _waitsHelper; // Ïîëå äëÿ õðàíåíèÿ ýêçåìïëÿðà âñïîìîãàòåëüíîãî êëàññà îæèäàíèé
-    private IWebElement _webElement; // Ïîëå äëÿ õðàíåíèÿ âåá-ýëåìåíòà
-    private Actions _actions; // Ïîëå äëÿ õðàíåíèÿ äåéñòâèé ñ âåá-ýëåìåíòàìè
+    
+    private IWebDriver _webDriver; // Для хранения экземпляра веб-драйвера
+    private WaitsHelper _waitsHelper; // Для работы с ожиданиями загрузки
+    private IWebElement _webElement; // Для хранения веб-элемента
+    private Actions _actions; // Для работы с действиями с веб-элементами
 
-    private UIElement(IWebDriver webDriver) // Ïðèâàòíûé êîíñòðóêòîð ñ ïàðàìåòðîì webDriver
+    private UIElement(IWebDriver webDriver) // Приватный конструктор класса UIElement, принимающий веб-драйвер в качестве параметра
     {
         _webDriver = webDriver;
         _waitsHelper = new WaitsHelper(webDriver, TimeSpan.FromSeconds(Configurator.WaitsTimeout));
         _actions = new Actions(webDriver);
     }
 
-    public UIElement(IWebDriver webDriver, By by) : this(webDriver) // Ïóáëè÷íûé êîíñòðóêòîð ñ ïàðàìåòðàìè webDriver è by, âûçûâàåò ïðèâàòíûé êîíñòðóêòîð
+    public UIElement(IWebDriver webDriver, By by) : this(webDriver) //Публичный конструктор класса UIElement, принимающий веб-драйвер и локатор элемента By, инициализирующий объект UIElement через вызов основного конструктора
     {
         _webElement = _waitsHelper.WaitForExists(by);
     }
 
-    public UIElement(IWebDriver webDriver, IWebElement webElement) : this(webDriver) // Ïóáëè÷íûé êîíñòðóêòîð ñ ïàðàìåòðàìè webDriver è webElement, âûçûâàåò ïðèâàòíûé êîíñòðóêòîð
+    public UIElement(IWebDriver webDriver, IWebElement webElement) : this(webDriver) //Публичный конструктор класса UIElement, принимающий веб-драйвер и веб-элемент, инициализирующий объект UIElement через вызов основного конструктора
     {
         _webElement = webElement;
     }
 
-    public IWebElement FindElement(By by) // Ìåòîä ïîèñêà âëîæåííîãî ýëåìåíòà ïî ëîêàòîðó By
+    public IWebElement FindElement(By by) //Метод для поиска и возвращения одного веб-элемента по локатору By
     {
         return _waitsHelper.WaitChildElement(_webElement, by);
     }
 
-    public UIElement FindUIElement(By by) // Ìåòîä ïîèñêà âëîæåííîãî ýëåìåíòà òèïà UIElement ïî ëîêàòîðó By
+    public UIElement FindUIElement(By by) //Метод для поиска и возвращения нового объекта UIElement, используя метод FindElement
     {
         return new UIElement(_webDriver, FindElement(by));
     }
 
-    public ReadOnlyCollection<IWebElement> FindElements(By by) // Ìåòîä ïîèñêà âñåõ âëîæåííûõ ýëåìåíòîâ ïî ëîêàòîðó By
+    public ReadOnlyCollection<IWebElement> FindElements(By by) //Метод для поиска и возвращения коллекции веб-элементов по локатору By
     {
         return _webElement.FindElements(by);
     }
 
-    public List<UIElement> FindUIElements(By by) // Ìåòîä ïîèñêà âñåõ âëîæåííûõ ýëåìåíòîâ òèïà UIElement ïî ëîêàòîðó By
+    public List<UIElement> FindUIElements(By by) //Метод для поиска и возвращения коллекции новых объектов UIElement, используя метод FindElements
     {
         var result = new List<UIElement>();
         foreach (var webElement in FindElements(by))
@@ -58,22 +58,22 @@ public class UIElement : IWebElement // Îáúÿâëåíèå êëàññà UIElem
         return result;
     }
 
-    public void Clear() // Ìåòîä î÷èñòêè òåêñòîâîãî ïîëÿ ýëåìåíòà
+    public void Clear() //Очищает текстовое поле веб-элемента
     {
         _webElement.Clear();
     }
 
-    public void SendKeys(string text) // Ìåòîä ââîäà òåêñòà â òåêñòîâîå ïîëå ýëåìåíòà
+    public void SendKeys(string text)  //Вводит указанный текст в текстовое поле веб-элемента
     {
         _webElement.SendKeys(text);
     }
 
-    public void Submit() // Ìåòîä îòïðàâêè ôîðìû, åñëè ýëåìåíò ÿâëÿåòñÿ ôîðìîé
+    public void Submit() //Отправляет форму, если веб-элемент является элементом формы
     {
         _webElement.Submit();
     }
 
-    public void Click() // Ìåòîä êëèêà ïî ýëåìåíòó, îáðàáàòûâàåò ElementNotInteractableException
+    public void Click() //Выполняет клик по веб-элементу. В случае, если элемент не доступен для взаимодействия, использует дополнительные методы для выполнения клика
     {
         try
         {
@@ -97,44 +97,44 @@ public class UIElement : IWebElement // Îáúÿâëåíèå êëàññà UIElem
         }
     }
 
-    public string GetAttribute(string attributeName) // Ìåòîä ïîëó÷åíèÿ çíà÷åíèÿ àòðèáóòà ýëåìåíòà
+    public string GetAttribute(string attributeName) //Возвращает значение указанного атрибута веб-элемента
     {
         return _webElement.GetAttribute(attributeName);
     }
 
-    public string GetDomAttribute(string attributeName) // Ìåòîä ïîëó÷åíèÿ çíà÷åíèÿ DOM-àòðèáóòà ýëåìåíòà
+    public string GetDomAttribute(string attributeName) //Возвращает значение указанного атрибута DOM веб-элемента
     {
         return _webElement.GetDomAttribute(attributeName);
     }
 
-    public string GetDomProperty(string propertyName) // Ìåòîä ïîëó÷åíèÿ çíà÷åíèÿ DOM-ñâîéñòâà ýëåìåíòà
+    public string GetDomProperty(string propertyName) //Возвращает значение указанного свойства DOM веб-элемента
     {
         return _webElement.GetDomProperty(propertyName);
     }
 
-    public string GetCssValue(string propertyName) // Ìåòîä ïîëó÷åíèÿ çíà÷åíèÿ CSS-ñâîéñòâà ýëåìåíòà
+    public string GetCssValue(string propertyName) //Возвращает значение указанного CSS свойства веб-элемента
     {
         return _webElement.GetCssValue(propertyName);
     }
 
-    public ISearchContext GetShadowRoot() // Ìåòîä ïîëó÷åíèÿ òåíåâîãî êîðíÿ ýëåìåíòà
+    public ISearchContext GetShadowRoot() //Возвращает корневой элемент теневого DOM веб-элемента, если он присутствует
     {
         return _webElement.GetShadowRoot();
     }
 
-    public void MoveToElement() // Ìåòîä ïåðåìåùåíèÿ ê ýëåìåíòó íà ñòðàíèöå
+    public void MoveToElement() //Прокручивает страницу к указанному веб-элементу
     {
         ((IJavaScriptExecutor)_webDriver).ExecuteScript("arguments[0].scrollIntoView(true);", _webElement);
     }
 
-    public void Hover() // Ìåòîä íàâåäåíèÿ êóðñîðà íà ýëåìåíò
+    public void Hover() //Выполняет наведение курсора на веб-элемент
     {
         _actions.MoveToElement(_webElement).Build().Perform();
     }
 
-    public string TagName => _webElement.TagName; // Ñâîéñòâî äëÿ ïîëó÷åíèÿ èìåíè òåãà ýëåìåíòà
+    public string TagName => _webElement.TagName; //Возвращает название тега веб-элемента
 
-    public string Text // Ñâîéñòâî äëÿ ïîëó÷åíèÿ òåêñòîâîãî ñîäåðæèìîãî ýëåìåíòà
+    public string Text //Возвращает текстовое содержимое веб-элемента. Логика определения текста может быть дополнительно настроена
     {
         get
         {
@@ -154,9 +154,9 @@ public class UIElement : IWebElement // Îáúÿâëåíèå êëàññà UIElem
         }
     }
 
-    public bool Enabled => _webElement.Enabled; // Ñâîéñòâî, óêàçûâàþùåå, äîñòóïåí ëè ýëåìåíò äëÿ âçàèìîäåéñòâèÿ
-    public bool Selected => _webElement.Selected; // Ñâîéñòâî, óêàçûâàþùåå, âûáðàí ëè ýëåìåíò
-    public Point Location => _webElement.Location; // Ñâîéñòâî äëÿ ïîëó÷åíèÿ ïîçèöèè ýëåìåíòà íà ñòðàíèöå
-    public Size Size => _webElement.Size; // Ñâîéñòâî äëÿ ïîëó÷åíèÿ ðàçìåðîâ ýëåìåíòà
-    public bool Displayed => _webElement.Displayed; // Ñâîéñòâî, óêàçûâàþùåå, îòîáðàæàåòñÿ ëè ýëåìåíò íà ñòðàíèöå
+    public bool Enabled => _webElement.Enabled; //Возвращает информацию о доступности веб-элемента для взаимодействия
+    public bool Selected => _webElement.Selected; //Возвращает информацию о выбранном состоянии веб-элемента
+    public Point Location => _webElement.Location; //Возвращает координаты расположения веб-элемента на странице
+    public Size Size => _webElement.Size; //Возвращает размеры веб-элемента
+    public bool Displayed => _webElement.Displayed; //Проверяет, отображается ли веб-элемент на странице
 }
